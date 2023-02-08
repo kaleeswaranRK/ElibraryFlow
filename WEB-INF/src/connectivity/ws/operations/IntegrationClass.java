bf2101cdd23438b1ffddd4ef04bfdc9033dfdcbb
kaleeswaranRK <kaleeswaran25597@gmail.com>
2023-02-08 15:03:19 +0530

    08-02-2023
    API-2 Achieved

--- WEB-INF/src/connectivity/ws/operations/IntegrationClass.java
+++ WEB-INF/src/connectivity/ws/operations/IntegrationClass.java
@@ -3,6 +3,8 @@ package connectivity.ws.operations;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
@@ -13,6 +15,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONObject;
+import org.json.JSONArray;
 import org.json.simple.parser.JSONParser;
 
 public class IntegrationClass {
@@ -21,7 +24,7 @@ public class IntegrationClass {
 			.setConnectTimeout(CONNECTION_TIMEOUT_MS).setSocketTimeout(CONNECTION_TIMEOUT_MS).build();
 	CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
-	public Object apiGet(String url) {
+	public List<JSONObject> apiGet(String url) {
 		try {
 			JSONObject jsonobj=null;
 			HttpGet Request = new HttpGet(url);
@@ -40,9 +43,60 @@ public class IntegrationClass {
 					jsonobj = new JSONObject(output);
 					if (jsonobj.get("status").toString().equalsIgnoreCase("200")) {
 						jsonobj = new JSONObject(jsonobj.get("data").toString());
-						Object object = jsonobj.get("Result");
-						if (object != null ) {
-							return object;
+						JSONArray result = (JSONArray) jsonobj.get("Result");
+						List<JSONObject> list=new ArrayList();
+						if (result != null ) {
+							for (int i = 0; i < result.length(); i++) {
+								JSONObject object = (JSONObject)result.get(i);
+								list.add(object);
+							}
+							return list;
+						} else {
+							return null;
+						}
+					} else {
+						return null;
+					}
+				}
+			}
+
+		}
+
+		catch (ClientProtocolException e) {
+
+			System.out.println(e);
+
+		} catch (IOException e) {
+
+			System.out.println(e);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return null;
+
+	}
+	public String apiGetPassword(String url) {
+		try {
+			JSONObject jsonobj=null;
+			HttpGet Request = new HttpGet(url);
+			Request.setHeader("Accept", "application/json");
+			Request.setHeader("Content-type", "application/json");
+			CloseableHttpResponse response = httpClient.execute(Request);
+
+			if (response.getStatusLine().getStatusCode() != 200) {
+				return null;
+			} else {
+				BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+				String output = br.readLine();
+				System.out.println("Output from Server .... \n");
+				if (output != null) {
+					System.out.println(output);
+					jsonobj = new JSONObject(output);
+					if (jsonobj.get("status").toString().equalsIgnoreCase("200")) {
+						jsonobj = new JSONObject(jsonobj.get("data").toString());
+						String password = jsonobj.get("Result").toString();
+						if (password != null ) {
+							return password;
 						} else {
 							return null;
 						}
